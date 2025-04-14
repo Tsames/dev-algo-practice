869a62831ff4eaad44fc4b5daf4e71dca4b9fd41
Tsames <tsames1@gmail.com>
2025-04-14 16:11:57 -0700

    Added O(n) time and space complexity solution. Also added helper functions for tests.

--- Algorithms/Linked_Lists/copy_linked_list_with_random_pointer.py
+++ Algorithms/Linked_Lists/copy_linked_list_with_random_pointer.py
@@ -41,12 +41,80 @@ Constraints:
 -104 <= Node.val <= 104
 Node.random is null or is pointing to some node in the linked list.
 """
-from linked_list_node import ListNode, create_linked_list, compare_linked_lists
 from typing import Optional
 
+
+class LinkedListNodeRandom:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = x
+        self.next = next
+        self.random = random
+
+def create_linked_list_random(nodes: list[list[int]]) -> Optional[LinkedListNodeRandom]:
+    if not nodes:
+        return None
+
+    nodes_map = {None : None}
+    dummy = LinkedListNodeRandom(None)
+    prev = dummy
+
+    for i in range(len(nodes)):
+        newNode = LinkedListNodeRandom(nodes[i][0])
+        nodes_map[i] = newNode
+        prev.next = newNode
+        prev = prev.next
+
+    for j in range(len(nodes)):
+        node = nodes_map[j]
+        node.random = nodes_map[nodes[j][1]]
+
+    return dummy.next
+
+def compare_linked_lists_random(l1: Optional[LinkedListNodeRandom], l2: Optional[LinkedListNodeRandom]) -> bool:
+    while l1 and l2:
+        if l1.val != l2.val:
+            return False
+        l1 = l1.next
+        l2 = l2.next
+    return l1 is None and l2 is None
+
 class Solution:
-    def copy_random_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return head
+    def copy_random_list(self, head: Optional[LinkedListNodeRandom]) -> Optional[LinkedListNodeRandom]:
+        """
+        The challenging part of this algorithm is the random property.
+        If the random property were not present, we would be able to create a deep copy by iterating through our
+        input linked list, creating a copy at each node and setting the previous nodes' next value to the new node we
+        just created.
+
+        However, the random property complicates this because we may not have created that node yet.
+        So, to overcome this issue we are going to iterate through our given linked list twice.
+        The first time, we will create deep copies, and to be able to access them easily and quickly (without having
+        to traverse our linked list each time) we will store them in hashmap with the original node as the key and
+        the deep copy node as the value.
+
+        Then, in the second iteration we will be able to set the random property with ease.
+        """
+
+        node_map = {None : None}
+        # Since we are iterating through our linked list twice, we need a separate variable so we can reset our
+        # iteration back to the beginning.
+        curr = head
+
+        # Pass number one, create the deep copy nodes
+        while curr:
+            node_map[curr] = LinkedListNodeRandom(curr.val)
+            curr = curr.next
+
+        # Pass Number two, setting random and next for our deep copy nodes using our hashmap
+        curr = head
+        while curr:
+            deep_copy = node_map[curr]
+
+            deep_copy.random = node_map[curr.random]
+            deep_copy.next = node_map[curr.next]
+            curr = curr.next
+
+        return node_map[head]
 
 solution = Solution()
 
@@ -79,10 +147,10 @@ test_cases = [
 ]
 
 for i, test in enumerate(test_cases, 1):
-    head = create_linked_list(test["input"])
-    expected = create_linked_list(test["expected"])
+    head = create_linked_list_random(test["input"])
+    expected = create_linked_list_random(test["expected"])
     result = solution.copy_random_list(head)
-    assert compare_linked_lists(result, expected), (
+    assert compare_linked_lists_random(result, expected), (
         f"Test {i} ({test['description']}) failed. "
         f"Expected: {test['expected']}, but got: {result}"
     )
