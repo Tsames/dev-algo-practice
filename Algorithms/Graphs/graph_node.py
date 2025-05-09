a931cf9a1347e810ed92a6ee8a48335ae0cf5daf
Tsames <tsames1@gmail.com>
2025-05-08 22:42:57 -0700

    Changes [wip]

--- Algorithms/Graphs/graph_node.py
+++ Algorithms/Graphs/graph_node.py
@@ -1,5 +1,5 @@
 class GraphNode:
-    def __init__(self, val=0, neighbors=None):
+    def __init__(self, val=0, neighbors=[]):
         self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
+        self.neighbors = neighbors
         
