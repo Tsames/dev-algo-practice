26a9d10f424ab4a26a3beae54882aed7bc175aaf
Tsames <tsames1@gmail.com>
2025-05-09 18:48:08 -0700

    Added working O(n) time and space complexity solution that passes all tests. Also added outline of the algorithm in comments.

--- Algorithms/Arrays/SlidingWindow/contains_duplciate_II.py
+++ Algorithms/Arrays/SlidingWindow/contains_duplciate_II.py
@@ -2,7 +2,8 @@
 https://leetcode.com/problems/contains-duplicate-ii/description/?envType=problem-list-v2&envId=sliding-window
 
 219. Contains Duplicate II
-Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such
+that nums[i] == nums[j] and abs(i - j) <= k.
 
 Example 1:
 Input: nums = [1,2,3,1], k = 3
@@ -22,8 +23,9 @@ Constraints:
 0 <= k <= 105
 """
 
+
 class Solution:
-    def containsNearbyDuplciate(self, nums: list[int], k:int) -> bool:
+    def containsNearbyDuplciate(self, nums: list[int], k: int) -> bool:
         """
         We are looking for two numbers that are equal to each other within our input list.
         We have the additional condition that the difference between these indices is smaller than k.
@@ -34,7 +36,37 @@ class Solution:
         We could use a hash map to store the values that we have seen.
         We could use two pointers, l and r, to represent our sliding window.
         We could add and remove values from our hashmap as we move our window along.
+
+        We iterate through our input list adding a count of the number we see to our hashmap, seen, along the way.
+        If the window becomes bigger than k, then we move our left pointer, l, over and remove a count of nums[l] from
+        seen.
+        If we then look at our hashmap and find a count that is greater than zero for the value of nums at our right
+        pointer, r, then we instantly return True because we've found numbers that satisfy the conditions.
+        We then add a count of nums[r] to our hashmap.
+        Finally, we return False if we finish the entire iteration and don't find numbers that satisfy our conditions.
+
+        O(n) Space and Time complexity.
+
+        We could maybe conditionally remove our key,value pairs from our hashmap and improve our space complexity to
+        O(k).
         """
+
+        seen = {}
+        l = 0
+
+        for r in range(len(nums)):
+            num = nums[r]
+
+            if r - l > k:
+                seen[nums[l]] -= 1
+                l += 1
+
+            if seen.get(num, 0) >= 1:
+                return True
+
+            seen[num] = 1 + seen.get(num, 0)
+
+        return False
 
 
 solution = Solution()
