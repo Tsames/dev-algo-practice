00bb405ae9d096813b883d1474bf74e5e35f9807
Tsames <tsames1@gmail.com>
2026-01-14 10:50:51 -0800

    Added other solution

--- Codewars/Dart/roman_numerals_decoder.dart
+++ Codewars/Dart/roman_numerals_decoder.dart
@@ -76,3 +76,24 @@ main() {
     expect(romanToInt('MCMXCIV'), equals(1994));
   });
 }
+
+/*
+    Similar solution, but a bit cleaner.
+
+    int romanToInt(String s){
+      Map toDec = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,};
+
+      int ans = 0;
+      List <int> temp = [];
+
+      s.split('').forEach((e) => temp.add(toDec[e]));
+
+      for (int i = 0; i < temp.length; i++) {
+        i == temp.length - 1 || temp[i] >= temp[i + 1]
+            ? ans += temp[i]
+            : ans -= temp[i];
+      }
+
+      return ans;
+    }
+ */
