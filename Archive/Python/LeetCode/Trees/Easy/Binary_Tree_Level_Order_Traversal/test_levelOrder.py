2ed043731dee3032336815379e5dff4ae86ece23
Tsames <tsames1@gmail.com>
2026-02-26 06:15:55 -0800

    Cleaning up

--- Archive/Python/LeetCode/Trees/Easy/Binary_Tree_Level_Order_Traversal/test_levelOrder.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from levelOrder import levelOrder
-
-#Import Linked List Data Structure
-import sys
-sys.path.append("/Users/tomames/dev/practice_repo/Python/Data_Structures/")
-from Binary_Tree import BinaryTree, BinaryTreeNode
--- README.md
+++
@@ -1,3 +0,0 @@
-# Tom Ames - Dev Practice
-
-This repository is intended to serve as a record of my work with data structures and algorithms with primarily python.
